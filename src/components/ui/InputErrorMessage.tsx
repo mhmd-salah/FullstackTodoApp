64f57f4cee6d075738a958c64ae08617351aea8a
mohamed salah <ms.mhmdsalah@gmail.com>
2024-07-20 17:14:00 +0200

    after electric on

--- src/components/ui/InputErrorMessage.tsx
+++ src/components/ui/InputErrorMessage.tsx
@@ -3,7 +3,7 @@ interface IProps {
 }
 const InputErrorMessage = ({ msg }: IProps) => {
   return msg ? (
-    <span className="block text-red-700 font-semibold text-sm">{msg}</span>
+    <span className="block text-red-600 font-semibold text-sm">{msg}</span>
   ) : null;
 };
 
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,38 +1,78 @@
 import { SubmitHandler, useForm } from "react-hook-form";
 import Button from "../components/ui/Button";
 import Input from "../components/ui/Input";
-import toast from "react-hot-toast";
+import InputErrorMessage from "../components/ui/InputErrorMessage";
 
-interface IformInput{
-  userName:string,
-  email:string,
-  password:string
+interface IformInput {
+  userName: string;
+  email: string;
+  password: string;
 }
 
-const  Register=()=>{
-  const { register,formState:{errors}, handleSubmit } = useForm<IformInput>();
-  const onSubmit:SubmitHandler<IformInput>=(data)=>console.log(data)
+const Register = () => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<IformInput>();
+  const onSubmit: SubmitHandler<IformInput> = (data) =>{
+    console.log(data);
+  } ;
+  console.log(errors)
+  return (
+    <div className="max-w-md mx-auto h-full mt-[40%] sm:mt-[15%]">
+      <h2 className="text-3xl mb-4">Register Now</h2>
+      <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <Input
+            placeholder="User Name"
+            {...register("userName", { required: true, maxLength: 20 })}
+          />
+          <div className="h-4">
+            {" "}
+            {errors?.userName && errors.userName.type === "required" && (
+              <InputErrorMessage msg="User Name is Required" />
+            )}
+            {errors?.userName && errors.userName.type === "minLength" && (
+              <InputErrorMessage msg="User Name is Required" />
+            )}
+          </div>
+        </div>
+        {/* email input  */}
+        <div>
+          <Input
+            placeholder="Email Address"
+            {...register("email", { required: "true", minLength: 5 })}
+          />
+          <div className="h-4">
+            {errors?.email && errors.email.type === "required" && (
+              <InputErrorMessage msg="Email is Required" />
+            )}
+            {errors?.email && errors.email.type === "minLength" && (
+              <InputErrorMessage msg="Min Length is 5 character" />
+            )}
+          </div>
+        </div>
 
-return (
-  <div className="max-w-md mx-auto h-full mt-[40%] sm:mt-[20%]">
-    <h2 className="text-3 xl mb-4">Register Now</h2>
-    <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
-      <Input
-        placeholder="User Name"
-        {...register("userName", { required: true, maxLength: 20 })}
-        aria-invalid={errors.userName ? "true" : "false"}
-        
-      />
-      {errors.userName?.type === "required" && (
-        <p role="alert" className="text-red-600">First name is required</p>
-      )}
-      <Input placeholder="Email Address" {...register("email")} />
-      <Input placeholder="Password" {...register("password")} />
-      <Button fullWidth type="submit">
-        Register
-      </Button>
-    </form>
-  </div>
-);
-}
+        <div>
+          <Input
+            placeholder="Password"
+            {...register("password", { required: "true" ,minLength:5})}
+          />
+          <div className="h-4">
+            {errors?.password && errors.password.type === "required" && (
+              <InputErrorMessage msg="password is Required" />
+            )}
+            {errors?.password && errors.password.type === "minLength" && (
+              <InputErrorMessage msg="Min Length is 5 character" />
+            )}
+          </div>
+        </div>
+        <Button fullWidth type="submit">
+          Register
+        </Button>
+      </form>
+    </div>
+  );
+};
 export default Register;
