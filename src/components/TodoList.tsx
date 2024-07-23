79d9fc1d42c7dee74985112ad87c52010adeb832
mohamed salah <ms.mhmdsalah@gmail.com>
2024-07-23 14:44:25 +0200

    use query hook

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,45 +1,40 @@
-import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 import Button from "./ui/Button";
+import axios from "axios";
 import axiosInstance from "../config/axios.config";
 
-interface ITodo{
-  id:number,
-  title:string
-}
 
 const TodoList = () => {
-  const [isLoading, setisLoading] = useState(false)
-  const [todos, setTodots] = useState<ITodo[]>([]);
   const loggedInUser = JSON.parse(
     localStorage.getItem("loggedInUser") as string
   );
   const userData = loggedInUser?.jwt;
-  useEffect(() => {
-    try {
-      axiosInstance
-        .get("/users/me?populate=todos", {
-          headers: {
-            Authorization: `Bearer ${userData}`,
-          },
-        })
-        .then((res) => setTodots(res.data.todos))
-        .catch((err) => console.log(err));
-    } catch (error) {
-      console.log(error);
+
+  const {isLoading,data,error}=useQuery({
+    queryKey:["repoData"],
+    queryFn:async()=>{
+      await axiosInstance.get("/users/me?populate=todos",{
+        headers:{
+          Authorization:`Bearer ${userData}`
+        }
+      });
     }
-  }, [userData]);
-  console.log(todos)
+  })
+  if(isLoading) return "loading..."
+  if(error) return "error as "+error?.message
   return (
     <div className="todo-colors space-y-3 ">
-      {todos.map((todo,idx) => (
-        <div className="flex justify-between bg-[#f6f7f8] items-center p-3 rounded-md"key={todo.id}>
-          <h3>{idx+1}- {todo.title}</h3>
-          <div className="flex space-x-2 ">
-            <Button className="w-[100px]">Edit</Button>
-            <Button className="w-[100px] DlB">Remove</Button>
-          </div>
+      <div
+        className="flex justify-between bg-[#f6f7f8] items-center p-3 rounded-md"
+      >
+        <h3>
+          {data?.title}
+        </h3>
+        <div className="flex space-x-2 ">
+          <Button className="w-[100px]">Edit</Button>
+          <Button className="w-[100px] DlB">Remove</Button>
         </div>
-      ))}
+      </div>
     </div>
   );
 };
