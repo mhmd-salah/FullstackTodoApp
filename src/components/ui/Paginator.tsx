c6fd0ffc6e2964e82bf5a61369a03af3ec6c11ee
mohamed salah <ms.mhmdsalah@gmail.com>
2024-07-30 06:29:57 +0200

    build pagination part 2

--- src/components/ui/Paginator.tsx
+++ src/components/ui/Paginator.tsx
@@ -16,7 +16,7 @@ const Paginator = ({
   onClickNext,
 }: Partial<IProps>) => {
   return (
-    <div className="flex justify-center items-center">
+    <div className="flex flex-col space-y-2 justify-center items-center">
       <p className="text-sm text-gray-600 mx-3">
         Page{" "}
         <span className="mx-1 font-semibold text-gray-900 text-md-1">
@@ -27,54 +27,55 @@ const Paginator = ({
         <span className="mx-1 font-semibold text-gray-900">{total}</span>{" "}
         Records
       </p>
-
-      <button
-        type="button"
-        className="w-[130px] bg-teal-600 text-white rounded-l-md border-r border border-gray-100 flex items-center justify-center px-4 h-10 me-3 text-base font-medium rounded-lg hover:bg-teal-700 hover:text-white  disabled:bg-gray-400 disabled:hover:bg-gray-400 disabled:cursor-not-allowed"
-        disabled={page === 1 || isLoading}
-        onClick={onClickPrev}
-      >
-        <svg
-          className="w-3.5 h-3.5 me-2 rtl:rotate-180"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 14 10"
+      <div className="flex ">
+        <button
+          type="button"
+          className="w-[130px] bg-teal-600 text-white rounded-l-md border-r border border-gray-100 flex items-center justify-center px-4 h-10 me-3 text-base font-medium rounded-lg hover:bg-teal-700 hover:text-white  disabled:bg-gray-400 disabled:hover:bg-gray-400 disabled:cursor-not-allowed"
+          disabled={page === 1 || isLoading}
+          onClick={onClickPrev}
         >
-          <path
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M13 5H1m0 0 4 4M1 5l4-4"
-          />
-        </svg>
-        Previous
-      </button>
+          <svg
+            className="w-3.5 h-3.5 me-2 rtl:rotate-180"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 10"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M13 5H1m0 0 4 4M1 5l4-4"
+            />
+          </svg>
+          Previous
+        </button>
 
-      <button
-        type="button"
-        className="w-[130px] bg-teal-600 text-white rounded-l-md border-r border border-gray-100 flex items-center justify-center px-4 h-10 me-3 text-base font-medium rounded-lg hover:bg-teal-700 hover:text-white dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white disabled:bg-gray-400 disabled:hover:bg-gray-400 disabled:cursor-not-allowed"
-        disabled={page === pageCount || isLoading}
-        onClick={onClickNext}
-      >
-        Next
-        <svg
-          className="w-3.5 h-3.5 ms-2 rtl:rotate-180"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 14 10"
+        <button
+          type="button"
+          className="w-[130px] bg-teal-600 text-white rounded-l-md border-r border border-gray-100 flex items-center justify-center px-4 h-10 me-3 text-base font-medium rounded-lg hover:bg-teal-7     00 hover:text-white dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white disabled:bg-gray-400 disabled:hover:bg-gray-400 disabled:cursor-not-allowed"
+          disabled={page === pageCount || isLoading}
+          onClick={onClickNext}
         >
-          <path
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M1 5h12m0 0L9 1m4 4L9 9"
-          />
-        </svg>
-      </button>
+          Next
+          <svg
+            className="w-3.5 h-3.5 ms-2 rtl:rotate-180"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 10"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M1 5h12m0 0L9 1m4 4L9 9"
+            />
+          </svg>
+        </button>
+      </div>
     </div>
   );
 };
--- src/pages/Todos.tsx
+++ src/pages/Todos.tsx
@@ -2,29 +2,29 @@ import { useState } from "react";
 import TodoSkeleton from "../components/TodoSkeleton";
 import Paginator from "../components/ui/Paginator";
 import useCustomQuery from "../hooks/useCustomQuery";
-import { Notyf } from "notyf";
+// import { Notyf } from "notyf";
 import "notyf/notyf.min.css";
 
 export default function Todos() {
-  // hooks 
+  // hooks
   const [page, setPage] = useState<number>(1);
-  const notyf = new Notyf({
-    types: [
-      {
-        type: "success",
-        background: "#0d9488",
-      },
-    ],
-  });
+  // const notyf = new Notyf({
+  //   types: [
+  //     {
+  //       type: "success",
+  //       background: "#0d9488",
+  //     },
+  //   ],
+  // });
 
   const loggedInUser = JSON.parse(
     localStorage.getItem("loggedInUser") as string
   );
   const userData = loggedInUser?.jwt;
 
-  const { data, isLoading } = useCustomQuery({
-    queryKey: ["paginated Todos",String(page)],
-    url: "/todos",
+  const { data, isLoading, isFetching } = useCustomQuery({
+    queryKey: [`todos-page-${page}`],
+    url: `/todos?pagination[pageSize]=10&pagination[page]=${page}`,
     config: {
       headers: {
         Authorization: `Bearer ${userData}`,
@@ -41,13 +41,13 @@ export default function Todos() {
       </div>
     );
 
-    // Handlers 
-    const onClickPrev = ()=>{
-      setPage(prev => prev -1)
-    }
-    const onClickNext = ()=>{
-      setPage(prev => prev + 1)
-    }
+  // Handlers
+  const onClickPrev = () => {
+    setPage((prev) => prev - 1);
+  };
+  const onClickNext = () => {
+    setPage((prev) => prev + 1);
+  };
   return (
     <>
       {data?.data?.length > 0 ? (
@@ -73,7 +73,14 @@ export default function Todos() {
         <h3 className="text-3xl p-4 text-center animate-pulse">No Todos Yet</h3>
       )}
       <div className="my-7">
-        <Paginator page={page} pageCount={4}  onClickPrev={onClickPrev} onClickNext={onClickNext}/>
+        <Paginator
+          page={page}
+          pageCount={data?.meta?.pagination?.pageCount}
+          onClickPrev={onClickPrev}
+          onClickNext={onClickNext}
+          total={data?.meta?.pagination?.total}
+          isLoading={isFetching||isLoading}
+        />
       </div>
     </>
   );
